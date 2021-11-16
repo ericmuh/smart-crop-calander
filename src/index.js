24cdfc5480fd20840c1b6a1f3114b8adb953679c
ericmuh <muhwezi1000@gmail.com>
2021-11-16 07:37:33 +0300

    update

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
-import "./assets/scss/smart-crop-calendar.scss";
+import "./assets/css/smart-crop-calendar.css";
 import "./assets/css/demo.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
